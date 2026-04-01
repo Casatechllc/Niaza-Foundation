31bc11e8682ee8b1a957c03fb659281565daa07e
A. A. Martinez (Casatechllc) <amartinez@casatechll.com>
2026-03-31 23:19:54 -0400

    fix navbar

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -159,7 +159,7 @@ const handleDonateClick = () => {
 </script>
 
 <style scoped>
-@reference "@/assets/css/main.css";
+@reference "@/assets/main.css";
 
 .mobile-nav-link {
   @apply -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50;
